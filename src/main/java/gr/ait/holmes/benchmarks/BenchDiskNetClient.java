3973a4bdea8c2b5cd799bc9008a4adbd585e1985
ioannischristou <ichr@ait.edu.gr>
2019-02-13 15:27:26 +0200

    adding benchmark classes.

--- src/main/java/gr/ait/holmes/benchmarks/BenchDiskNetClient.java
+++ src/main/java/gr/ait/holmes/benchmarks/BenchDiskNetClient.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package benchdisknet;
+package gr.ait.holmes.benchmarks;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
