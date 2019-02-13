846f40d3b751b32d09303368ce259bfbe2af3437
ioannischristou <ichr@ait.edu.gr>
2019-02-13 15:24:35 +0200

    adding benchmark classes.

---
+++ src/main/java/gr/ait/holmes/benchmarks/BenchDiskNet.java
@@ -0,0 +1,13 @@
+/*
+ * Code is distibuted as Open Source, under the LGPL2 license, without any waranty of fitness of use.
+ */
+
+package gr.ait.holmes.benchmarks;
+
+/**
+ *
+ * @author itc
+ */
+public class BenchDiskNet {
+  
+}
